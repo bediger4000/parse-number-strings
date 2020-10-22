ab2c27a6b2dfe8821c198b5d2e7fa14075e50dda
Bruce Ediger <bediger8@gmail.com>
2020-10-22 14:39:02 -0600

    Loop over all arguments

--- v1.go
+++ v1.go
@@ -12,17 +12,17 @@ import (
 )
 
 func main() {
-	stringNumber := os.Args[1]
+	for _, stringNumber := range os.Args[1:] {
+		if _, err := strconv.Atoi(stringNumber); err == nil {
+			fmt.Printf("%q represents a number\n", stringNumber)
+			continue
+		}
 
-	if _, err := strconv.Atoi(stringNumber); err == nil {
-		fmt.Printf("%q represents a number\n", stringNumber)
-		return
+		if _, err := strconv.ParseFloat(stringNumber, 64); err == nil {
+			fmt.Printf("%q represents a number\n", stringNumber)
+			continue
+		}
+
+		fmt.Printf("%q does not represent a number\n", stringNumber)
 	}
-
-	if _, err := strconv.ParseFloat(stringNumber, 64); err == nil {
-		fmt.Printf("%q represents a number\n", stringNumber)
-		return
-	}
-
-	fmt.Printf("%q does not represent a number\n", stringNumber)
 }
